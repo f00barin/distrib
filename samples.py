8c4161e1bbeb3aa915e35b77f347f8f306ee6878
Pranava Swaroop <pranava.madhyastha@gmail.com>
2013-03-21 19:20:13 +0100

    default options modified, cleaned

--- samples.py
+++ samples.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python
+
 import numpy as np
 import scipy.sparse as ss
+from tabulate import tabulate 
+
 
 wordlist = 'all_wordnet.txt'
 content = [word.strip() for word in open(wordlist)]
 
 
-default = None
 def sampledef(matrix, format, words):
     word_dict = {}
     farray = np.array(matrix.todense().argsort(axis=1)[::, ::-1][:,
@@ -48,8 +51,31 @@ def samplespl(matrix, format, words):
  
 
 
+default = None
 
-#def pprintdicts(dict1, dict2=default, dict3=default, dict4=default):
+def pprint(source1, source2=default, source3=default):
+    simlist = []
+    for key in source1:
+        ll = []
+        ll.append(key)
+        ll.append(source1[key])
+        if source2 != None:
+            ll.append(source2[key])
+        if source3 != None:
+            ll.append(source3[key])
+
+        simlist.append(ll)
+
+    if source3 != None and  source2 != None:
+        print tabulate(simlist, ["word", "source1", "source2", "source3"], tablefmt="grid")
+    elif source2 != None:
+        print tabulate(simlist, ["word", "source1", "source2"], tablefmt="grid")
+    else:
+        print tabulate(simlist, ["word", "source1"], tablefmt="grid")
+
+
+
+
 
 
 
